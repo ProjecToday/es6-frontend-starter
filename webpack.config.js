108a8f29427bcb48ec19d815b5ccb9b601698ce1
timqian <timqian92@qq.com>
2016-02-26 13:23:50 +0800

    change start script to webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   entry: "./src/index.js",
+  devtool: 'inline-source-map', // for source map
   output: {
     path: './dist',
+    publicPath: "/dist",  // for dev server
     filename: "bundle.js"
   },
   module: {
